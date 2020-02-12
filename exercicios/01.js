24d095f9d76cd582861cca38012934c1442989dc
gisellesouza <gisellesouz.ti@gmail.com>
2020-02-12 20:25:34 -0300

    exercicio 01

---
+++ exercicios/01.js
@@ -0,0 +1,8 @@
+function calculadora(a, b) {
+  console.log(`${a} + ${b} = ${a + b}`);
+  console.log(`${a} - ${b} = ${a - b}`);
+  console.log(`${a} * ${b} = ${a * b}`);
+  console.log(`${a} / ${b} = ${a / b}`);
+}
+
+calculadora(10, 5);
