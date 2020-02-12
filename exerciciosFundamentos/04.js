a03e9e7522f381681715763d9b17c63890eba387
gisellesouza <gisellesouz.ti@gmail.com>
2020-02-12 20:51:30 -0300

    exercicio 04

---
+++ exerciciosFundamentos/04.js
@@ -0,0 +1,7 @@
+const divisao = (a, b) => {
+  console.log(`O resultado de (${a} / ${b}) é ${a / b}. `);
+  console.log(`O resto de (${a} / ${b}) é ${a % b}. `);
+};
+
+divisao(10, 2);
+divisao(5, 2);
