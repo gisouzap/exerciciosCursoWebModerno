7eeb8ea138523e82941f05a93be3559bdc006864
gisellesouza <gisellesouz.ti@gmail.com>
2020-02-13 20:02:35 -0300

    exercicio 10

--- exerciciosFundamentos/10.js
+++ exerciciosFundamentos/10.js
@@ -1,6 +1,3 @@
-/*Crie uma função que verifica se um número inteiro passado como parêmetro é divisível por 3 e retorne true
-ou false.*/
-
 const verifica = numero => {
   return numero % 3 == 0 ? true : false;
 };
