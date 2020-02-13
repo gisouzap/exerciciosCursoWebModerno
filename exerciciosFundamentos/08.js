4004c0112c603843c6545010f1ee2f9ac51ed0a1
gisellesouza <gisellesouz.ti@gmail.com>
2020-02-13 19:34:33 -0300

    exercicio 08

---
+++ exerciciosFundamentos/08.js
@@ -0,0 +1,21 @@
+let partidas = [10, 20, 20, 8, 25, 3, 0, 30, 4];
+
+const temporada = partidas => {
+  let pior = 1;
+  let maiorpontuacao = partidas[0];
+  let menorpontuacao = partidas[0];
+  let recorde = 0;
+
+  for (let i = 0; i <= partidas.length; i++) {
+    if (partidas[i] > maiorpontuacao) {
+      maiorpontuacao = partidas[i];
+      recorde++;
+    } else if (partidas[i] < menorpontuacao) {
+      menorpontuacao = partidas[i];
+      pior = i + 1;
+    }
+  }
+  return [recorde, piorJogo];
+};
+
+console.log(temporada(partidas));
