2c7897f5c00b6a51c5dad745495584fe3e63d19d
gisellesouza <gisellesouz.ti@gmail.com>
2020-02-13 18:44:13 -0300

    exercicio 06

--- exerciciosFundamentos/06.js
+++ exerciciosFundamentos/06.js
@@ -1,7 +1,3 @@
-/*Elabore duas funções que recebem três parâmetros: capital inicial, taxa de juros e tempo de aplicação. A
-primeira função retornará o montante da aplicação financeira sob o regime de juros simples e a segunda
-retornará o valor da aplicação sob o regime de juros compostos.*/
-
 const jurosSimples = (capital, taxa, meses) => {
   return `R$: ${capital * (taxa / 100) * meses + capital}`;
 };
