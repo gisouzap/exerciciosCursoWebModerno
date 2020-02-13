2ee0d490f25171e35b92c7f9c8217aee1beff5e7
gisellesouza <gisellesouz.ti@gmail.com>
2020-02-13 20:56:13 -0300

    exercicio 19

--- exerciciosFundamentos/19.js
+++ exerciciosFundamentos/19.js
@@ -1,15 +1,3 @@
-/*O cardápio de uma lanchonete é o seguinte:
-Código Descrição do Produto Preço
-100 Cachorro Quente R$ 3,00
-200 Hambúrguer Simples R$ 4,00
-300 Cheeseburguer R$ 5,50
-400 Bauru R$ 7,50
-500 Refrigerante R$ 3,50
-600 Suco R$ 2,80
-Implemente uma função que receba como parâmetros o código do item pedido, a quantidade e calcule o valor
-a ser pago por aquele lanche. Considere que a cada execução somente será calculado um item. Use o
-comando switch. Crie um caso default para produto não existente.*/
-
 const lanchonete = (pedido, qtd) => {
   switch (pedido) {
     case 100:
