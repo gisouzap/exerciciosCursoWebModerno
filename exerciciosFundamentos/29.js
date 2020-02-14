e1d9ff205333f7ab486e5a2bd05f79f4341a8e10
gisellesouza <gisellesouz.ti@gmail.com>
2020-02-14 16:27:46 -0300

    exercicio 29

--- exerciciosFundamentos/29.js
+++ exerciciosFundamentos/29.js
@@ -1,7 +1,3 @@
-/*Utilizando a estrutura de repetição for, faça uma função que percorra um vetor e conte quantos números
-deste vetor estão no intervalo [10,20] (repare que o intervalo é fechado, ou seja, inclui o 10 e o 20) e quantos
-deles estão fora do intervalo, escrevendo estas informações.*/
-
 function intervalo(vetor) {
   let dentro = 0;
   let fora = 0;
