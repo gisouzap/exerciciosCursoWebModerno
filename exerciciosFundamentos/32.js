f620a60ed82ff7923576e35f3b3b97bb0cb51db6
gisellesouza <gisellesouz.ti@gmail.com>
2020-02-14 16:49:07 -0300

    exercicio 32

--- exerciciosFundamentos/32.js
+++ exerciciosFundamentos/32.js
@@ -1,5 +1,3 @@
-/*Construir um algoritmo que calcule a média aritmética dos valores de um vetor de inteiros.*/
-
 function media(vetor) {
   let soma = 0;
   for (let i = 0; i < vetor.length; i++) {
